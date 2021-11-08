64f846b5324d106d1f95c715f1fd34514eba7cc4
unknown <gd_dada@esi.dz>
2021-11-08 22:06:48 +0100

    created list + ratio + responsive + delete buttono

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/movie/Movie';
+import { useState } from 'react';
+import { movies$ } from './api/movies';
+import { Progress, Spin } from 'antd';
 
 function App() {
+  // {Promise.all([movies]).then(data=>{console.log(data)})}
+
+  const [movies, setMovies] = useState({})
+  const [isloading, setIsloading] = useState(true)
+  useState(() => {
+    // setIsloading(true)
+    movies$.then(movie => {
+      setMovies(movie)
+    });
+    // setIsloading(false)
+  }, [])
+
+  // movies$.then(movie => {
+  //   setMovies(movie)
+  // });
+
+  const handleDelete = (id) =>{
+    setMovies(movies.filter(movie => movie.id != id))
+    console.log('number is deleted',id)
+  }
+
+  const handleLikeDislike = (id) =>{
+    setMovies(movies.map(movie => movie.id == id ? {...movie,likes:movie.likes+1} : {movie}))
+    // setMovies(movies.map(movie=>{
+    //   if(movie?.id == id){
+    //     if(movie?.likedalready==false){
+    //       return{...movie,likes:movie.likes+1, likedalready:false}
+    //     }else{
+    //       return{...movie,likes:movie.likes-1,likedalready:true }
+    //     }
+    //   }
+    // }))
+    console.log('number is liked/disliked',id)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!movies.length==0 ? <div className='movies' >
+        {movies.map((movie,index) => (
+          <Movie key={index} movie={movie} handleDelete={handleDelete} handleLikeDislike={handleLikeDislike} />
+        ))}
+      </div> : <div><Spin size="large" /></div>}
     </div>
   );
 }
---
+++ src/components/movie/Movie.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { movies$ } from "../../api/movies"
+import './movie.css'
+// import LinearProgress from '@mui/material/LinearProgress';
+import { Progress, Button, Switch } from 'antd';
+
+
+const Movie = ({ movie, handleDelete, handleLikeDislike }) => {
+
+    return (
+        <div className='movie' >
+            <h3> {movie?.title} </h3>
+            <h5> {movie?.category} </h5>
+            <p> {movie?.likes} || {movie?.dislikes} </p>
+            <Switch defaultChecked={false} onClick={()=>handleLikeDislike(movie?.id)} />
+            <Progress strokeLinecap="square" percent={movie?.likes / (movie?.likes + movie?.dislikes) * 100} showInfo={false} status='normal' />
+            <div className='movie__delete__button' >
+                <Button danger onClick={()=>handleDelete(movie?.id)} >Delete</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Movie
