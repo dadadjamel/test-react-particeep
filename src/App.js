a9bba8997fd7fdc49e5a219f2a491acae8a4dc45
unknown <gd_dada@esi.dz>
2021-11-08 23:00:53 +0100

    pagination added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Movie from './components/movie/Movie';
 import { useState } from 'react';
 import { movies$ } from './api/movies';
-import { Progress, Spin } from 'antd';
+import { Button, Progress, Spin } from 'antd';
 
 function App() {
   // {Promise.all([movies]).then(data=>{console.log(data)})}
 
   const [movies, setMovies] = useState({})
   const [isloading, setIsloading] = useState(true)
+  const [numberOfItemInPage, setNumberOfItemInPage] = useState(4)
+  const [pages] = useState(Math.round(movies.length / numberOfItemInPage));
+  const [currentPage, setCurrentPage] = useState(1)
   useState(() => {
     // setIsloading(true)
     movies$.then(movie => {
@@ -18,38 +21,60 @@ function App() {
     // setIsloading(false)
   }, [])
 
+  useState(() => {
+  }, [movies])
+
+
+
   // movies$.then(movie => {
   //   setMovies(movie)
   // });
 
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     setMovies(movies.filter(movie => movie.id != id))
-    console.log('number is deleted',id)
+    console.log('number is deleted', id)
   }
 
-  const handleLikeDislike = (id) =>{
-    setMovies(movies.map(movie => movie.id == id ? {...movie,likes:movie.likes+1} : {movie}))
-    // setMovies(movies.map(movie=>{
-    //   if(movie?.id == id){
-    //     if(movie?.likedalready==false){
-    //       return{...movie,likes:movie.likes+1, likedalready:false}
-    //     }else{
-    //       return{...movie,likes:movie.likes-1,likedalready:true }
-    //     }
-    //   }
-    // }))
-    console.log('number is liked/disliked',id)
+  const handleLikeDislike = (id) => {
+    // setMovies(movies.map(movie => movie.id == id ? {...movie,likes:movie.likes+1} : movie))
+    setMovies(movies.map(movie => {
+      if (movie?.id == id) {
+        if (movie?.likedalready == false) {
+          return { ...movie, likes: movie.likes + 1, test: 'da' }
+        } else {
+          return { ...movie, likes: movie.likes - 1, likedalready: true }
+        }
+      } else return movie
+    }))
+    console.log('number is liked/disliked', id)
   }
 
 
 
   return (
     <div className="App">
-      {!movies.length==0 ? <div className='movies' >
-        {movies.map((movie,index) => (
+
+
+      {!movies.length == 0 ? <div className='movies' >
+        {movies.slice(currentPage * numberOfItemInPage - numberOfItemInPage, currentPage * numberOfItemInPage).map((movie, index) => (
           <Movie key={index} movie={movie} handleDelete={handleDelete} handleLikeDislike={handleLikeDislike} />
         ))}
       </div> : <div><Spin size="large" /></div>}
+
+
+      <div>
+        <Button onClick={()=>setNumberOfItemInPage(4)} >4</Button>
+        <Button onClick={()=>setNumberOfItemInPage(8)} >8</Button>
+        <Button onClick={()=>setNumberOfItemInPage(12)} >12</Button>
+      </div>
+
+
+      <div>
+        <Button onClick={()=>setCurrentPage(currentPage=>currentPage-1)} disabled={currentPage==1} >Back</Button>
+        <Button onClick={()=>setCurrentPage(currentPage=>currentPage+1)} disabled={currentPage==Math.ceil(movies.length / numberOfItemInPage)}>Next</Button>
+      </div>
+
+
     </div>
   );
 }
--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -8,15 +8,18 @@ import { Progress, Button, Switch } from 'antd';
 const Movie = ({ movie, handleDelete, handleLikeDislike }) => {
 
     return (
+        
         <div className='movie' >
             <h3> {movie?.title} </h3>
             <h5> {movie?.category} </h5>
-            <p> {movie?.likes} || {movie?.dislikes} </p>
+            <h5> {movie?.test} </h5>
+            <p> 👍 {movie?.likes} 👎 {movie?.dislikes} </p>
             <Switch defaultChecked={false} onClick={()=>handleLikeDislike(movie?.id)} />
             <Progress strokeLinecap="square" percent={movie?.likes / (movie?.likes + movie?.dislikes) * 100} showInfo={false} status='normal' />
             <div className='movie__delete__button' >
                 <Button danger onClick={()=>handleDelete(movie?.id)} >Delete</Button>
             </div>
+
         </div>
     )
 }
